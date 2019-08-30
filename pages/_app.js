0ce78cc059fad908ad4a22528dc44650eda86dad
EINazare <emanuelioannazare@gmail.com>
2019-08-30 19:41:21 +0300

    - trying to add a nice spinner for page change

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import App from 'next/app';
 import Head from 'next/head';
+import Router from 'next/router'
+import NProgress from 'nprogress'
 
 import "../assets/scss/material-kit-react.scss?v=1.8.0";
 
+Router.events.on('routeChangeStart', url => {
+  console.log(`Loading: ${url}`)
+  document.body.classList.add("page-transition-spinner");
+
+})
+Router.events.on('routeChangeComplete', () => {document.body.classList.remove("page-transition-spinner");})
+Router.events.on('routeChangeError', () => {document.body.classList.remove("page-transition-spinner");})
+
 export default class MyApp extends App {
+  static async getInitialProps ({ Component, router, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return { pageProps }
+  }
   render() {
     const { Component, pageProps } = this.props;
 
