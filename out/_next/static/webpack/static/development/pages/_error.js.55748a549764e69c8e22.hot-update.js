fd2fb8cdcef9ffc2c335c627df11605c7577c545
EINazare <emanuelioannazare@gmail.com>
2019-09-09 16:32:28 +0300

    - new version of out for deploy

---
+++ out/_next/static/webpack/static/development/pages/_error.js.55748a549764e69c8e22.hot-update.js
@@ -0,0 +1,222 @@
+webpackHotUpdate("static/development/pages/_error.js",{
+
+/***/ "./node_modules/@babel/runtime-corejs2/core-js/array/from.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/core-js/is-iterable.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/core-js/set.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/arrayWithoutHoles.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/iterableToArray.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/nonIterableSpread.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/toConsumableArray.js":
+false,
+
+/***/ "./node_modules/core-js/library/fn/array/from.js":
+false,
+
+/***/ "./node_modules/core-js/library/fn/is-iterable.js":
+false,
+
+/***/ "./node_modules/core-js/library/fn/set.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_array-from-iterable.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_array-methods.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_array-species-constructor.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_array-species-create.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_collection-strong.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_collection-to-json.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_collection.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_create-property.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_set-collection-from.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_set-collection-of.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_validate-collection.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/core.is-iterable.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es6.array.from.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es6.set.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es7.set.from.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es7.set.of.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es7.set.to-json.js":
+false,
+
+/***/ "./node_modules/next-server/dist/lib/amp-context.js":
+false,
+
+/***/ "./node_modules/next-server/dist/lib/amp.js":
+false,
+
+/***/ "./node_modules/next-server/dist/lib/head-manager-context.js":
+false,
+
+/***/ "./node_modules/next-server/dist/lib/head.js":
+false,
+
+/***/ "./node_modules/next-server/dist/lib/side-effect.js":
+false,
+
+/***/ "./node_modules/next-server/head.js":
+false,
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_error&absolutePagePath=%2FUsers%2Fmanu%2FDeveloper%2Fnext-material-kit%2Fpages%2F_error.js!./":
+/*!**************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_error&absolutePagePath=%2FUsers%2Fmanu%2FDeveloper%2Fnext-material-kit%2Fpages%2F_error.js ***!
+  \**************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/_error", function() {
+      var page = __webpack_require__(/*! ./pages/_error.js */ "./pages/_error.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/_error.js */ "./pages/_error.js", function() {
+          if(!next.router.components["/_error"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/_error.js */ "./pages/_error.js")
+          next.router.update("/_error", updatedPage.default || updatedPage)
+        })
+      }
+      return { page: page.default || page }
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_error&absolutePagePath=next%2Fdist%2Fpages%2F_error!./":
+false,
+
+/***/ "./node_modules/next/dist/pages/_error.js":
+false,
+
+/***/ "./pages/_error.js":
+/*!*************************!*\
+  !*** ./pages/_error.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return _error; });
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_classCallCheck__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/classCallCheck */ "./node_modules/@babel/runtime-corejs2/helpers/esm/classCallCheck.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_createClass__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/createClass */ "./node_modules/@babel/runtime-corejs2/helpers/esm/createClass.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_possibleConstructorReturn__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/possibleConstructorReturn */ "./node_modules/@babel/runtime-corejs2/helpers/esm/possibleConstructorReturn.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_getPrototypeOf__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/getPrototypeOf */ "./node_modules/@babel/runtime-corejs2/helpers/esm/getPrototypeOf.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_assertThisInitialized__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/assertThisInitialized */ "./node_modules/@babel/runtime-corejs2/helpers/esm/assertThisInitialized.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_inherits__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/inherits */ "./node_modules/@babel/runtime-corejs2/helpers/esm/inherits.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_defineProperty__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/defineProperty */ "./node_modules/@babel/runtime-corejs2/helpers/esm/defineProperty.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_7___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_7__);
+/* harmony import */ var next_router__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! next/router */ "./node_modules/next/dist/client/router.js");
+/* harmony import */ var next_router__WEBPACK_IMPORTED_MODULE_8___default = /*#__PURE__*/__webpack_require__.n(next_router__WEBPACK_IMPORTED_MODULE_8__);
+
+
+
+
+
+
+
+var _jsxFileName = "/Users/manu/Developer/next-material-kit/pages/_error.js";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_7___default.a.createElement;
+
+
+
+var _error =
+/*#__PURE__*/
+function (_Component) {
+  Object(_babel_runtime_corejs2_helpers_esm_inherits__WEBPACK_IMPORTED_MODULE_5__["default"])(_error, _Component);
+
+  function _error() {
+    var _getPrototypeOf2;
+
+    var _this;
+
+    Object(_babel_runtime_corejs2_helpers_esm_classCallCheck__WEBPACK_IMPORTED_MODULE_0__["default"])(this, _error);
+
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    _this = Object(_babel_runtime_corejs2_helpers_esm_possibleConstructorReturn__WEBPACK_IMPORTED_MODULE_2__["default"])(this, (_getPrototypeOf2 = Object(_babel_runtime_corejs2_helpers_esm_getPrototypeOf__WEBPACK_IMPORTED_MODULE_3__["default"])(_error)).call.apply(_getPrototypeOf2, [this].concat(args)));
+
+    Object(_babel_runtime_corejs2_helpers_esm_defineProperty__WEBPACK_IMPORTED_MODULE_6__["default"])(Object(_babel_runtime_corejs2_helpers_esm_assertThisInitialized__WEBPACK_IMPORTED_MODULE_4__["default"])(_this), "componentDidMount", function () {
+      next_router__WEBPACK_IMPORTED_MODULE_8___default.a.push("/components");
+    });
+
+    return _this;
+  }
+
+  Object(_babel_runtime_corejs2_helpers_esm_createClass__WEBPACK_IMPORTED_MODULE_1__["default"])(_error, [{
+    key: "render",
+    value: function render() {
+      return __jsx("div", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 10
+        },
+        __self: this
+      });
+    }
+  }]);
+
+  return _error;
+}(react__WEBPACK_IMPORTED_MODULE_7__["Component"]);
+
+
+
+/***/ }),
+
+/***/ 1:
+/*!******************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2F_error&absolutePagePath=%2FUsers%2Fmanu%2FDeveloper%2Fnext-material-kit%2Fpages%2F_error.js ***!
+  \******************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2F_error&absolutePagePath=%2FUsers%2Fmanu%2FDeveloper%2Fnext-material-kit%2Fpages%2F_error.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_error&absolutePagePath=%2FUsers%2Fmanu%2FDeveloper%2Fnext-material-kit%2Fpages%2F_error.js!./");
+
+
+/***/ })
+
+})
+//# sourceMappingURL=_error.js.55748a549764e69c8e22.hot-update.js.map
