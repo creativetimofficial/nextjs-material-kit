1ff1782041443e421ea3fcd26db37c28b9840b8e
EINazare <emanuelioannazare@gmail.com>
2019-09-10 14:35:49 +0300

    - <<material kit react>> to <<nextjs material kit>>

--- pages/profilepage.js
+++ pages/profilepage.js
@@ -47,7 +47,7 @@ export default function ProfilePage(props) {
     <div>
       <Header
         color="transparent"
-        brand="Material Kit React"
+        brand="NextJS Material Kit"
         rightLinks={<HeaderLinks />}
         fixed
         changeColorOnScroll={{
