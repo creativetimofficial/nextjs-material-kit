8a32f8b50311fb35ff3a91cad55d4cbee979da62
EINazare <emanuelioannazare@gmail.com>
2019-09-11 12:07:27 +0300

    - Added copyrights on js and scss as well

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,20 @@
+/*!
+
+=========================================================
+* NextJS Material Kit v1.0.0 based on Material Kit Free - v2.0.2 (Bootstrap 4.0.0 Final Edition) and Material Kit React v1.8.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/nextjs-material-kit
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/nextjs-material-kit/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "next/app";
