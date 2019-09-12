dfe0b1b0a70c8099fb4b724133edc5248ba391c8
EINazare <emanuelioannazare@gmail.com>
2019-09-12 12:24:47 +0300

    ready to deploy

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 module.exports = withPlugins([[withSass], [withImages]], {
   assetPrefix:
-    process.env.NODE_ENV === "production" ? "/next-material-kit" : "",
+    process.env.NODE_ENV === "production" ? "/nextjs-material-kit" : "",
   webpack(config, options) {
     config.resolve.modules.push(path.resolve("./"));
     return config;
