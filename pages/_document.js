28cba439895d1f57a1deea670a0a8ead25a34486
EINazare <emanuelioannazare@gmail.com>
2019-09-13 11:38:09 +0300

    ready to deploy

--- pages/_document.js
+++ pages/_document.js
@@ -109,7 +109,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           ></script>
           <script
             dangerouslySetInnerHTML={{
-              __html: `// Facebook Pixel Code Don't Delete
+              __html: `<script>// Facebook Pixel Code Don't Delete
 !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
 n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
@@ -122,7 +122,7 @@ try{
 
 }catch(err) {
   console.log('Facebook Track Error:', err);
-}`
+}</script>`
             }}
           ></script>
         </Head>
