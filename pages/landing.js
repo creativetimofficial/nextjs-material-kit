e63cef63f4ca4019227eac2c097a466c5bcba592
EINazare <emanuelioannazare@gmail.com>
2019-09-13 08:07:11 +0300

    ready to deploy

--- pages/landing.js
+++ pages/landing.js
@@ -43,7 +43,7 @@ export default function LandingPage(props) {
         }}
         {...rest}
       />
-    <Parallax filter responsive image={require("assets/img/landing-bg.jpg")}>
+      <Parallax filter responsive image={require("assets/img/landing-bg.jpg")}>
         <div className={classes.container}>
           <GridContainer>
             <GridItem xs={12} sm={12} md={6}>
