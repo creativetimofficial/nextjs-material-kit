ede918b936993c53078f8ca315162822be6d8ce1
EINazare <emanuelioannazare@gmail.com>
2019-09-13 11:17:14 +0300

    - share twitter button

--- pages-sections/Components-Sections/SectionDownload.js
+++ pages-sections/Components-Sections/SectionDownload.js
@@ -86,7 +86,7 @@ export default function SectionDownload() {
           </GridContainer>
           <Button
             color="twitter"
-            href="https://twitter.com/home?status=NextJS%20Material%20Kit%20is%20a%20NextJS%20UI%20Kit%20with%20a%20fresh,%20new%20design%20inspired%20by%20Google's%20Material%20Design,%20based%20on%20Material%20Kit%20React%20by%20Creative%20Tim%20and%20Material%20UI%20%E2%9D%A4%EF%B8%8Fhttps%3A//demos.creative-tim.com/nextjs-material-kit/%20%23nextjs%20%23material%20%23design%20%23ui%20%23kit%20%23free%20%20via%20%40CreativeTim"
+            href="https://twitter.com/share?url=https%3A%2F%2Fdemos.creative-tim.com%2Fnextjs-material-kit%2Fcomponents&text=NextJS%20Material%20Kit&via=Creative%20Tim&hashtags=nextjs%20%23creativetim%20%23materialdesign"
             target="_blank"
           >
             <i className={classes.socials + " fab fa-twitter"} /> Tweet
