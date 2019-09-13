bf138bd61b22bd6c40a8c76c049ba3d9c769ebc3
EINazare <emanuelioannazare@gmail.com>
2019-09-13 09:11:03 +0300

    ready to deploy

--- pages/documentation/buildtools.js
+++ pages/documentation/buildtools.js
@@ -64,47 +64,6 @@ export default function BuildTools() {
             <tr>
               <td className="text-center">2</td>
               <td>
-                <code className="highlighter-rouge">start</code>
-              </td>
-              <td>
-                <code className="highlighter-rouge">npm start</code>
-              </td>
-              <td className="text-left">
-                This will determinate the start of your development server.
-              </td>
-              <td className="text-right">
-                <a
-                  href="https://facebook.github.io/create-react-app/docs/available-scripts#npm-start?ref=creativetim"
-                  target="_blank"
-                >
-                  Read more
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td className="text-center">3</td>
-              <td>
-                <code className="highlighter-rouge">build</code>
-              </td>
-              <td>
-                <code className="highlighter-rouge">npm run build</code>
-              </td>
-              <td className="text-left">
-                This will create a build directory with a production build of
-                your app.
-              </td>
-              <td className="text-right">
-                <a
-                  href="https://facebook.github.io/create-react-app/docs/available-scripts#npm-run-build?ref=creativetim"
-                  target="_blank"
-                >
-                  Read more
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td className="text-center">4</td>
-              <td>
                 <code className="highlighter-rouge">test</code>
               </td>
               <td>
@@ -117,7 +76,48 @@ export default function BuildTools() {
               </td>
               <td className="text-right">
                 <a
-                  href="https://facebook.github.io/create-react-app/docs/available-scripts#npm-test?ref=creativetim"
+                  href="https://github.com/zeit/next.js/issues/1434?ref=creativetim"
+                  target="_blank"
+                >
+                  Read more
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td className="text-center">3</td>
+              <td>
+                <code className="highlighter-rouge">dev</code>
+              </td>
+              <td>
+                <code className="highlighter-rouge">npm run dev</code>
+              </td>
+              <td className="text-left">
+                This will determinate the start of your development server.
+              </td>
+              <td className="text-right">
+                <a
+                  href="https://nextjs.org/docs#quick-start?ref=creativetim"
+                  target="_blank"
+                >
+                  Read more
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td className="text-center">4</td>
+              <td>
+                <code className="highlighter-rouge">build</code>
+              </td>
+              <td>
+                <code className="highlighter-rouge">npm run build</code>
+              </td>
+              <td className="text-left">
+                This will create a build directory with a production build of
+                your app.
+              </td>
+              <td className="text-right">
+                <a
+                  href="https://nextjs.org/docs#quick-start?ref=creativetim"
                   target="_blank"
                 >
                   Read more
@@ -127,19 +127,17 @@ export default function BuildTools() {
             <tr>
               <td className="text-center">5</td>
               <td>
-                <code className="highlighter-rouge">eject</code>
+                <code className="highlighter-rouge">start</code>
               </td>
               <td>
-                <code className="highlighter-rouge">npm run eject</code>
+                <code className="highlighter-rouge">npm start</code>
               </td>
               <td className="text-left">
-                This command will remove the single build dependency from your
-                project. (We do not recommend using this command. Our product
-                may not work after using this command.)
+                This will determinate the start of your production server.
               </td>
               <td className="text-right">
                 <a
-                  href="https://facebook.github.io/create-react-app/docs/available-scripts#npm-run-eject?ref=creativetim"
+                  href="https://nextjs.org/docs#quick-start?ref=creativetim"
                   target="_blank"
                 >
                   Read more
@@ -158,96 +156,7 @@ export default function BuildTools() {
                 This command will remove the{" "}
                 <code className="highlighter-rouge">node_modules</code> folder
                 and <code className="highlighter-rouge">package_lock.json</code>{" "}
-                file, and will install a fresh copy of them.
-              </td>
-              <td className="text-right" />
-            </tr>
-            <tr>
-              <td className="text-center">7</td>
-              <td>
-                <code className="highlighter-rouge">compile-sass</code>
-              </td>
-              <td>
-                <code className="highlighter-rouge">npm run compile-sass</code>
-              </td>
-              <td className="text-left">
-                This command will compile the scss files located inside{" "}
-                <code className="highlighter-rouge">src/assets/scss/*</code>{" "}
-                into{" "}
-                <code className="highlighter-rouge">
-                  src/assets/css/black-dashboard-pro-react.css
-                </code>
-                .
-              </td>
-              <td className="text-right" />
-            </tr>
-            <tr>
-              <td className="text-center">8</td>
-              <td>
-                <code className="highlighter-rouge">minify-sass</code>
-              </td>
-              <td>
-                <code className="highlighter-rouge">npm run minify-sass</code>
-              </td>
-              <td className="text-left">
-                This command will compile and minify the scss files located
-                inside{" "}
-                <code className="highlighter-rouge">src/assets/scss/*</code>{" "}
-                into{" "}
-                <code className="highlighter-rouge">
-                  src/assets/css/black-dashboard-pro-react.min.css
-                </code>
-                .
-              </td>
-              <td className="text-right" />
-            </tr>
-            <tr>
-              <td className="text-center">9</td>
-              <td>
-                <code className="highlighter-rouge">map-sass</code>
-              </td>
-              <td>
-                <code className="highlighter-rouge">npm run map-sass</code>
-              </td>
-              <td className="text-left">
-                This command will compile and map the scss files located inside{" "}
-                <code className="highlighter-rouge">src/assets/scss/*</code>{" "}
-                into{" "}
-                <code className="highlighter-rouge">
-                  src/assets/css/black-dashboard-pro-react.css.map
-                </code>
-                .
-              </td>
-              <td className="text-right" />
-            </tr>
-            <tr>
-              <td className="text-center">10</td>
-              <td>
-                <code className="highlighter-rouge">build-package-css</code>
-              </td>
-              <td>
-                <code className="highlighter-rouge">
-                  npm run build-package-css
-                </code>
-              </td>
-              <td className="text-left">
-                This will copy the compiled version of the scss file from the
-                assets folder in the dist folder in order for you to publish the
-                dist folder to npm
-              </td>
-              <td className="text-right" />
-            </tr>
-            <tr>
-              <td className="text-center">11</td>
-              <td>
-                <code className="highlighter-rouge">build-package</code>
-              </td>
-              <td>
-                <code className="highlighter-rouge">npm run build-package</code>
-              </td>
-              <td className="text-left">
-                This will prepare the product for npm distribution (Caution -
-                the product was not thought as a npm distribution)
+                file, will install a fresh copy of them and will determinate the start of your development server.
               </td>
               <td className="text-right" />
             </tr>
