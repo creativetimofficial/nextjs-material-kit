3246a3de8d06ed4bac541e08df25d3423b3b242a
EINazare <emanuelioannazare@gmail.com>
2020-05-08 10:51:47 +0300

    refactor: change scripts to newer ones

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,47 @@ class MyDocument extends Document {
     return (
       <html lang="en">
         <Head>
+          <style
+            dangerouslySetInnerHTML={{
+              __html: `.async-hide { opacity: 0 !important} `
+            }}
+          ></style>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+(a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+})(window,document.documentElement,'async-hide','dataLayer',4000,
+{'GTM-K9BGS8K':true});`
+            }}
+          ></script>
+          {/*Analytics-Optimize Snippet*/}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-46172202-22', 'auto', {allowLinker: true});
+ga('set', 'anonymizeIp', true);
+ga('require', 'GTM-K9BGS8K');
+ga('require', 'displayfeatures');
+ga('require', 'linker');
+ga('linker:autoLink', ["2checkout.com","avangate.com"]);`
+            }}
+          ></script>
+          {/*End Analytics-Optimize Snippet*/}
+          {/*Google Tag Manager*/}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NKDMSK6');`
+            }}
+          ></script>
+          {/*§nd Google Tag Manager*/}
           <meta charSet="utf-8" />
           <meta
             name="viewport"
@@ -96,45 +137,12 @@ class MyDocument extends Document {
             href="https://use.fontawesome.com/releases/v5.0.10/css/all.css"
             rel="stylesheet"
           />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NKDMSK6');`
-            }}
-          ></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `// Facebook Pixel Code Don't Delete
-!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-document,'script','//connect.facebook.net/en_US/fbevents.js');
-
-try{
-  fbq('init', '111649226022273');
-  fbq('track', "PageView");
-
-}catch(err) {
-  console.log('Facebook Track Error:', err);
-}`
-            }}
-          ></script>
         </Head>
         <body>
           {/* Google Tag Manager (noscript) */}
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-NKDMSK6"
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
-            ></iframe>
-          </noscript>
-          {/* End Google Tag Manager (noscript) */}
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NKDMSK6"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+{/* End Google Tag Manager (noscript) */}
           <div id="page-transition"></div>
           <Main />
           <NextScript />
