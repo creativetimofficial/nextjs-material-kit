a84dee2620d1ab80583df14729291b8ad6d9093c
cedric-sd <cedricsoares813@gmail.com>
2021-02-12 20:25:28 -0300

    update classname from material-kit

--- assets/jss/nextjs-material-kit.js
+++ assets/jss/nextjs-material-kit.js
@@ -45,7 +45,7 @@ const transition = {
   transition: "all 0.33s cubic-bezier(0.685, 0.0473, 0.346, 1)"
 };
 
-const conatinerFluid = {
+const containerFluid = {
   paddingRight: "15px",
   paddingLeft: "15px",
   marginRight: "auto",
@@ -53,7 +53,7 @@ const conatinerFluid = {
   width: "100%"
 };
 const container = {
-  ...conatinerFluid,
+  ...containerFluid,
   "@media (min-width: 576px)": {
     maxWidth: "540px"
   },
@@ -233,7 +233,7 @@ export {
   drawerWidth,
   transition,
   container,
-  conatinerFluid,
+  containerFluid,
   boxShadow,
   card,
   defaultFont,
