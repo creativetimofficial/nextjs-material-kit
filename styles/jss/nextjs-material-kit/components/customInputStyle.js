f6d7a66ae45f6c09e94779a321bd9d77ac8b49fd
EINazare <emanuelioannazare@gmail.com>
2021-05-13 16:58:14 +0300

    fix: move assets to styles

--- styles/jss/nextjs-material-kit/components/customInputStyle.js
+++ styles/jss/nextjs-material-kit/components/customInputStyle.js
@@ -3,7 +3,7 @@ import {
   dangerColor,
   successColor,
   defaultFont
-} from "assets/jss/nextjs-material-kit.js";
+} from "styles/jss/nextjs-material-kit.js";
 
 const customInputStyle = {
   disabled: {
