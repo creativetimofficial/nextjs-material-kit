f1f26e6d8a483bfcdf74fa924a58a9bb43d754ee
EINazare <emanuelioannazare@gmail.com>
2021-05-13 17:02:25 +0300

    style: run prettier

--- styles/jss/nextjs-material-kit/pages/componentsSections/carouselStyle.js
+++ styles/jss/nextjs-material-kit/pages/componentsSections/carouselStyle.js
@@ -2,13 +2,13 @@ import { container } from "styles/jss/nextjs-material-kit.js";
 
 const carouselStyle = {
   section: {
-    padding: "70px 0"
+    padding: "70px 0",
   },
   container,
   marginAuto: {
     marginLeft: "auto !important",
-    marginRight: "auto !important"
-  }
+    marginRight: "auto !important",
+  },
 };
 
 export default carouselStyle;
