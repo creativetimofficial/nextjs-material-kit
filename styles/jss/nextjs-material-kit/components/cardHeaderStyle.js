9a719558489b13a637b50056bf8de64b420677ca
EINazare <emanuelioannazare@gmail.com>
2021-05-13 16:58:14 +0300

    fix: move assets to styles

--- styles/jss/nextjs-material-kit/components/cardHeaderStyle.js
+++ styles/jss/nextjs-material-kit/components/cardHeaderStyle.js
@@ -4,7 +4,7 @@ import {
   dangerCardHeader,
   infoCardHeader,
   primaryCardHeader
-} from "assets/jss/nextjs-material-kit.js";
+} from "styles/jss/nextjs-material-kit.js";
 
 const cardHeaderStyle = {
   cardHeader: {
