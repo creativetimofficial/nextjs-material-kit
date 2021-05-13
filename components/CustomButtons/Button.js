3791c6d8f1cc2b96a512a4e6688bf87e922dadde
EINazare <emanuelioannazare@gmail.com>
2021-05-13 16:52:20 +0300

    fix: solve image imports and style imports

--- components/CustomButtons/Button.js
+++ components/CustomButtons/Button.js
@@ -10,7 +10,7 @@ import Button from "@material-ui/core/Button";
 
 // core components
 
-import buttonStyle from "assets/jss/nextjs-material-kit/components/buttonStyle.js";
+import buttonStyle from "styles/jss/nextjs-material-kit/components/buttonStyle.js";
 
 const makeComponentStyles = makeStyles(() => ({
   ...buttonStyle
