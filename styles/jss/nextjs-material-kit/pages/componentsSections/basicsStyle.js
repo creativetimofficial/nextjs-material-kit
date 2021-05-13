6ca9321db7ff0cccad7ef6a09d1866abb7757084
EINazare <emanuelioannazare@gmail.com>
2021-05-13 17:02:25 +0300

    style: run prettier

--- styles/jss/nextjs-material-kit/pages/componentsSections/basicsStyle.js
+++ styles/jss/nextjs-material-kit/pages/componentsSections/basicsStyle.js
@@ -3,29 +3,29 @@ import customCheckboxRadioSwitch from "styles/jss/nextjs-material-kit/customChec
 
 const basicsStyle = {
   sections: {
-    padding: "70px 0"
+    padding: "70px 0",
   },
   container,
   title: {
     ...title,
     marginTop: "30px",
     minHeight: "32px",
-    textDecoration: "none"
+    textDecoration: "none",
   },
   space50: {
     height: "50px",
-    display: "block"
+    display: "block",
   },
   space70: {
     height: "70px",
-    display: "block"
+    display: "block",
   },
   icons: {
     width: "17px",
     height: "17px",
-    color: "#FFFFFF"
+    color: "#FFFFFF",
   },
-  ...customCheckboxRadioSwitch
+  ...customCheckboxRadioSwitch,
 };
 
 export default basicsStyle;
