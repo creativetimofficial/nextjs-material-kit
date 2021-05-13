31cf108ef78f3ff256864d151a8637b4d5b33234
EINazare <emanuelioannazare@gmail.com>
2021-05-13 16:58:15 +0300

    fix: move assets to styles

--- styles/jss/nextjs-material-kit/components/typographyStyle.js
+++ styles/jss/nextjs-material-kit/components/typographyStyle.js
@@ -5,7 +5,7 @@ import {
   successColor,
   warningColor,
   dangerColor
-} from "assets/jss/nextjs-material-kit.js";
+} from "styles/jss/nextjs-material-kit.js";
 
 const typographyStyle = {
   defaultFontStyle: {
