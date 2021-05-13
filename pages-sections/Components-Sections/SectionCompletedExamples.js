950f496c675d1b44c15d11546cddfb896b46c509
EINazare <emanuelioannazare@gmail.com>
2021-05-13 16:52:22 +0300

    fix: solve image imports and style imports

--- pages-sections/Components-Sections/SectionCompletedExamples.js
+++ pages-sections/Components-Sections/SectionCompletedExamples.js
@@ -7,7 +7,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
 
-import styles from "assets/jss/nextjs-material-kit/pages/componentsSections/completedStyle.js";
+import styles from "styles/jss/nextjs-material-kit/pages/componentsSections/completedStyle.js";
 
 const useStyles = makeStyles(styles);
 
