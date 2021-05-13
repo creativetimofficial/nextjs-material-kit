78c0767e0b99659b44d629d9b2858d9740e5a4b4
EINazare <emanuelioannazare@gmail.com>
2021-05-13 16:52:21 +0300

    fix: solve image imports and style imports

--- pages-sections/Components-Sections/SectionBasics.js
+++ pages-sections/Components-Sections/SectionBasics.js
@@ -22,7 +22,7 @@ import CustomLinearProgress from "components/CustomLinearProgress/CustomLinearPr
 import Paginations from "components/Pagination/Pagination.js";
 import Badge from "components/Badge/Badge.js";
 
-import styles from "assets/jss/nextjs-material-kit/pages/componentsSections/basicsStyle.js";
+import styles from "styles/jss/nextjs-material-kit/pages/componentsSections/basicsStyle.js";
 
 const useStyles = makeStyles(styles);
 
