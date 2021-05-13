03207e7117c42a8263d0e1dcb431f7415e614160
EINazare <emanuelioannazare@gmail.com>
2021-05-13 16:52:21 +0300

    fix: solve image imports and style imports

--- components/Parallax/Parallax.js
+++ components/Parallax/Parallax.js
@@ -7,7 +7,7 @@ import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 
 // core components
-import styles from "assets/jss/nextjs-material-kit/components/parallaxStyle.js";
+import styles from "styles/jss/nextjs-material-kit/components/parallaxStyle.js";
 
 const useStyles = makeStyles(styles);
 
