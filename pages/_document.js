77e05e47888ca0ce60d5b09a5c74c4e93556d90a
EINazare <emanuelioannazare@gmail.com>
2021-05-13 16:52:22 +0300

    fix: solve image imports and style imports

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,19 @@
 import React from "react";
-import Document, { Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheets } from "@material-ui/styles";
 
 class MyDocument extends Document {
   render() {
     return (
-      <html lang="en">
+      <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
           <meta name="theme-color" content="#000000" />
-          <link rel="shortcut icon" href={require("assets/img/favicon.png")} />
+          <link rel="shortcut icon" href="/img/favicon.png" />
           <link
             rel="apple-touch-icon"
             sizes="76x76"
-            href={require("assets/img/apple-icon.png")}
+            href="/img/apple-icon.png"
           />
           {/* Fonts and icons */}
           <link
@@ -35,7 +31,7 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
