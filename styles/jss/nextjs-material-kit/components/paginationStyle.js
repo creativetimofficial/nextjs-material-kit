ffc11cf945e08d3708bf3ffee32da2755e626acb
EINazare <emanuelioannazare@gmail.com>
2021-05-13 16:58:15 +0300

    fix: move assets to styles

--- styles/jss/nextjs-material-kit/components/paginationStyle.js
+++ styles/jss/nextjs-material-kit/components/paginationStyle.js
@@ -7,7 +7,7 @@ import {
   warningColor,
   dangerColor,
   roseColor
-} from "assets/jss/nextjs-material-kit.js";
+} from "styles/jss/nextjs-material-kit.js";
 
 const paginationStyle = {
   pagination: {
