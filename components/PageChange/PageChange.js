24a9ef91c910201acc4f807e0a320d1ef00e2e50
EINazare <emanuelioannazare@gmail.com>
2021-05-13 16:52:21 +0300

    fix: solve image imports and style imports

--- components/PageChange/PageChange.js
+++ components/PageChange/PageChange.js
@@ -5,7 +5,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
 // core components
-import { infoColor, title } from "assets/jss/nextjs-material-kit.js";
+import { infoColor, title } from "styles/jss/nextjs-material-kit.js";
 
 const useStyles = makeStyles({
   progress: {
