92124bced875ab0b8f2990e2f2d04c0ece5b29e1
Oleg La <oleglambin@gmail.com>
2023-03-27 18:02:47 +0300

    fix replace color #9c27b0 to #2d97d3

--- styles/jss/nextjs-material-kit.js
+++ styles/jss/nextjs-material-kit.js
@@ -90,7 +90,7 @@ const defaultFont = {
   lineHeight: "1.5em",
 };
 
-const primaryColor = "#9c27b0";
+const primaryColor = "#2d97d3";
 const warningColor = "#ff9800";
 const dangerColor = "#f44336";
 const successColor = "#4caf50";
