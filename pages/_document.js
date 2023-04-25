41b381109cecdc957d890532b8e2a602e6e7bb83
Sajad Ahmad Nawabi <nawabisajadahmad@gmail.com>
2023-04-26 02:26:17 +0430

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -25,6 +25,13 @@ class MyDocument extends Document {
             href="https://use.fontawesome.com/releases/v5.0.10/css/all.css"
             rel="stylesheet"
           />
+          {/* <!-- Nepcha Analytics (nepcha.com) -->
+          <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. --> */}
+          <script
+            defer
+            data-site="YOUR_DOMAIN_HERE"
+            src="https://api.nepcha.com/js/nepcha-analytics.js"
+          ></script>
         </Head>
         <body>
           <div id="page-transition"></div>
