161d474e1e91c0a473c9c5d98843575d08a78632
Maisam Afshar <maisam.afshar@gmail.com>
2023-08-22 14:21:45 +0330

    fix routing issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 /*!
 
 =========================================================
-* NextJS Material Kit v1.2.1 based on Material Kit Free - v2.0.2 (Bootstrap 4.0.0 Final Edition) and Material Kit React v1.8.0
+* NextJS Material Kit v1.2.2 based on Material Kit Free - v2.0.2 (Bootstrap 4.0.0 Final Edition) and Material Kit React v1.8.0
 =========================================================
 
 * Product Page: https://www.creative-tim.com/product/nextjs-material-kit
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/nextjs-material-kit/blob/main/LICENSE.md)
 
 * Coded by Creative Tim
@@ -16,42 +16,20 @@
 
 */
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "next/app";
 import Head from "next/head";
-import Router from "next/router";
-
-import PageChange from "/components/PageChange/PageChange.js";
-
 import "/styles/scss/nextjs-material-kit.scss?v=1.2.0";
-
-Router.events.on("routeChangeStart", (url) => {
-  console.log(`Loading: ${url}`);
-  document.body.classList.add("body-page-transition");
-  ReactDOM.render(
-    <PageChange path={url} />,
-    document.getElementById("page-transition")
-  );
-});
-Router.events.on("routeChangeComplete", () => {
-  ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
-  document.body.classList.remove("body-page-transition");
-});
-Router.events.on("routeChangeError", () => {
-  ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
-  document.body.classList.remove("body-page-transition");
-});
 
 export default class MyApp extends App {
   componentDidMount() {
     let comment = document.createComment(`
 
 =========================================================
-* NextJS Material Kit v1.2.1 based on Material Kit Free - v2.0.2 (Bootstrap 4.0.0 Final Edition) and Material Kit React v1.8.0
+* NextJS Material Kit v1.2.2 based on Material Kit Free - v2.0.2 (Bootstrap 4.0.0 Final Edition) and Material Kit React v1.8.0
 =========================================================
 
 * Product Page: https://www.creative-tim.com/product/nextjs-material-kit
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/nextjs-material-kit/blob/main/LICENSE.md)
 
 * Coded by Creative Tim
